0a14525514b2f08f620ec583464efc6eb51d96b6
Alexandr Siryj <theobroma222@gmail.com>
2022-01-26 12:24:17 +0200

    feat: BackToTop

---
+++ src/@components/UI/BackToTop.tsx
@@ -0,0 +1,36 @@
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Zoom from '@mui/material/Zoom';
+import Box from '@mui/material/Box';
+import React from 'react';
+
+const BackToTop: React.FC = ({ children }) => {
+  const trigger = useScrollTrigger();
+
+  // TODO: mb React.MouseEvent<HTMLElement>
+  const handleClick = (event: any) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+};
+
+export default BackToTop;
